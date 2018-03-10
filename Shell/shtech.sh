e021fa52ba3301b27356059eda72b864f566b84b
Jerry <Jerry981028@gmail.com>
2018-03-10 19:23:24 +0800

    shtech.sh: Add interface option

--- Shell/shtech.sh
+++ Shell/shtech.sh
@@ -1,28 +1,8 @@
 #!/bin/sh
 
-echo "
 
-
- _______  _______  _______  _        _______ _________ _______ 
-(  ____ \(  ____ \(  ____ \| \    /\(  ____ )\__   __/(  ____ \
-
-| (    \/| (    \/| (    \/|  \  / /| (    )|   ) (   | (    \/
-| |      | (__    | (__    |  (_/ / | (____)|   | |   | (__    
-| | ____ |  __)   |  __)   |   _ (  |  _____)   | |   |  __)   
-| | \_  )| (      | (      |  ( \ \ | (         | |   | (      
-| (___) || (____/\| (____/\|  /  \ \| )      ___) (___| (____/\
-
-(_______)(_______/(_______/|_/    \/|/       \_______/(_______/
-
-"
-
-
-echo $geekpie
-echo "Shanghaitech Login Shell"
-echo "上科大登陆器shell版本"
-num=$#
-if [ "$num" = "2" ]; then
-       result=` curl -k -H "Content-Type: application/x-www-form-urlencoded" -X POST -s --data "userName=$1&password=$2&hasValidateCode=false&authLan=zh_CN"  --cookie "JSESSIONID=D56359E00B58C7877668AAB44B3BFE31"  https://controller.shanghaitech.edu.cn:8445/PortalServer//Webauth/webAuthAction\!login.action`
+main(){
+       result=`${curl} -k -H "Content-Type: application/x-www-form-urlencoded" -X POST -s --data "userName=$1&password=$2&hasValidateCode=false&authLan=zh_CN"  --cookie "JSESSIONID=D56359E00B58C7877668AAB44B3BFE31"  https://controller.shanghaitech.edu.cn:8445/PortalServer//Webauth/webAuthAction\!login.action`
         resultSucess=`echo "$result" | sed "s/.*success\W*\(\w*\)\W*token.*/\1/g"`
         #echo "$resultSucess"
         if [ "$resultSucess"x = "true"x ]; then
@@ -52,7 +32,37 @@ if [ "$num" = "2" ]; then
                 echo "Log in failed , Please try again"
                 exit
                 fi
+}
+
+echo "
+
+
+ _______  _______  _______  _        _______ _________ _______ 
+(  ____ \(  ____ \(  ____ \| \    /\(  ____ )\__   __/(  ____ \
+
+| (    \/| (    \/| (    \/|  \  / /| (    )|   ) (   | (    \/
+| |      | (__    | (__    |  (_/ / | (____)|   | |   | (__    
+| | ____ |  __)   |  __)   |   _ (  |  _____)   | |   |  __)   
+| | \_  )| (      | (      |  ( \ \ | (         | |   | (      
+| (___) || (____/\| (____/\|  /  \ \| )      ___) (___| (____/\
+
+(_______)(_______/(_______/|_/    \/|/       \_______/(_______/
+
+"
+
+
+echo $geekpie
+echo "Shanghaitech Login Shell"
+echo "上科大登陆器shell版本"
+num=$#
+if [ "$num" = "2" ]; then
+curl="curl"
+main $*
+elif [ "$num" = "3" ]; then
+curl="curl --interface ${3} --dns-interface ${3}"
+main $1 $2
 else
-    echo "使用方法 ./shtech.sh 用户名 密码"
-    echo "Usage ./shtech.sh username password"
+    echo "使用方法 ./shtech.sh 用户名 密码 (接口)"
+    echo "Usage ./shtech.sh username password (interface)"
+    exit 1
 fi
